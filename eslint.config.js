987c20155b2509977e29372755efc7942fd6034e
Martiks <cappa.giuliano@gmail.com>
2025-12-16 15:50:10 -0300

    fix(eslint): corrected ESLint configuration file

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,26 @@
-export default {
-  env: {
-    node: true,
-    es2022: true,
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
   },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2022,
-    sourceType: 'module',
-    project: './tsconfig.json',
+  {
+    ignores: ['node_modules/**', 'dist/**'],
   },
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  rules: {
-    'prettier/prettier': 'error',
-    '@typescript-eslint/no-unused-vars': ['warn'],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-  },
-  ignorePatterns: ['node_modules/', 'dist/'],
-};
+];
