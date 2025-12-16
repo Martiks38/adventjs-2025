4fcf992235af3bce4e4c3972e8f014610c2563d3
Martiks <cappa.giuliano@gmail.com>
2025-12-16 01:49:09 -0300

    build(dependecies): add packages and configurations

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+export default {
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+  ignorePatterns: ['node_modules/', 'dist/'],
+};
