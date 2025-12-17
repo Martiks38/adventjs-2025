04d6a405a3bc6a2d0353a92adeb29bb1aefbc904
Martiks <cappa.giuliano@gmail.com>
2025-12-16 23:50:31 -0300

    test(challenge-02): add unit tests

--- src/02/02.ts
+++ src/02/02.ts
@@ -1,5 +1,5 @@
-function manufactureGifts(
-  giftsToProduce: Array<{ toy: string; quantity: number }>,
+export function manufactureGifts(
+  giftsToProduce: Array<{ toy: string; quantity: unknown }>,
 ): string[] {
   return giftsToProduce.flatMap<string>(({ toy, quantity }) => {
     if (typeof quantity !== 'number' || quantity <= 0) return [];
